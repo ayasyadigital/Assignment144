1ceed626d4ee3152425a8e15fb76f7fa142f70a4
Niket Goel <niket03@gmail.com>
2017-12-13 22:25:28 +0530

    Initial commit

---
+++ app/src/main/java/com/niketgoel/assignment144/PermissionsFragment.java
@@ -0,0 +1,23 @@
+package com.niketgoel.assignment144;
+
+/**
+ * Created by niketgoel on 13/12/17.
+ */
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class PermissionsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, null);
+    }
+}
